640f86e151bccf15c575a8aff51c533e929d8393
DaELHoser <DaELHoser@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-04-10 19:17:28 +0000

    Downloads Pictures on Background CANNOT DOWNLOAD 20+ pictures @ once

--- Image Notification/src/com/image/DownImages.java
+++ Image Notification/src/com/image/DownImages.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.ref.SoftReference;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -22,7 +23,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -33,11 +33,10 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
-import android.os.IBinder;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class DownImages extends BroadcastReceiver {
@@ -54,16 +53,54 @@ public class DownImages extends BroadcastReceiver {
 	public URL reviewImageURL;
 	private boolean isImage = false;
 	private static final String TAG = "PRANJAL";
+	public Context newContext;
+	int numOfPictures=0;
+	static int picsB4=0;
+	ArrayList<String> picNames=new ArrayList<String>(20);
+	int picNum=5;
+	boolean go=true;//false means downloading. true means done
+	int savePic;
+	
+	Bitmap daPicture;
 
 	/*----------------------------New----------------------------*/
-	
+	///NOTE: THERE ARE TWO getServerData fuctions overloaded
+	//ONE TO GET # OF PICTURES ONLINE OTHER TO DOWNLOAD THEM
 	@Override
 	public void onReceive(Context context, Intent intent) 
 	{
-		Toast.makeText(context, "Alarm worked.", Toast.LENGTH_LONG).show();
-		getServerData(GETMAX,context);	
-		changeURLStr(url + "image001",context);
 
+		newContext=context;
+		numOfPictures=getServerData(GETMAX,context);	//gets humber of pictures
+		
+		if(numOfPictures != picsB4)//new pictures can be less or > than before
+		{//NOTE:IT IS CHECKING FOR CHANGES SO IF ONE ADDED THEN SUB WONT WORK
+			getServerData(KEY_121);//gets names of pictures and saves on picNames
+			
+	
+			//picNum=picNames.size();
+				picNum=20;
+				savePic=20;
+			while(picNum>0)
+			{				
+				changeURLStr(picNames.get(picNum-1),context);//download images
+				//resetPurgeTimer();
+			//	Bitmap bitmap = getBitmapFromCache(url + picNames.get(picNum-1));
+				//while(bitmap==null)
+				//{
+			//		   Toast.makeText(context, "Waiting", Toast.LENGTH_LONG).show();
+				//	   bitmap = getBitmapFromCache(url + picNames.get(picNum-1));
+					//clearCache();
+				//	changeURLStr(url + picNames.get(picNum-1),context);//download images
+			//	}
+				
+				picNum--;
+
+			}
+			picsB4=numOfPictures;
+		}
+		//getServerData(KEY_121);//gets names of pictures and saves on picNames
+	//	changeURLStr(url + picNames.get(15),context);//download images
 	}
 
 
@@ -71,16 +108,17 @@ public class DownImages extends BroadcastReceiver {
 	
 	//new new new new new new new new new new new new new new new new new new new new new new
 
-
-	private String getServerData(String returnString,Context context) 
+	//this fuction CALCULATES # OF PICTURES ONLINE NOT NAMES!!!!
+	private int getServerData(String returnString,Context context) 
 	{
 
 	   InputStream is = null;
-		Toast.makeText(context, "here", Toast.LENGTH_LONG).show();
+	   Toast.makeText(context, "Checking for New Pictures", Toast.LENGTH_LONG).show();
 
 	   String result = "";
 	   String result1 = "";
-	   String single[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
+	   int numofPics=0;
+	  // String single[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
 	   int i = 0;
 	    //the year data to send
 	    ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
@@ -105,7 +143,7 @@ public class DownImages extends BroadcastReceiver {
 
 	    //convert response to string
 	    try{
-			Toast.makeText(context, "2", Toast.LENGTH_LONG).show();
+			Toast.makeText(context, "Getting names", Toast.LENGTH_LONG).show();
 
 	            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
 	            StringBuilder sb = new StringBuilder();
@@ -115,8 +153,10 @@ public class DownImages extends BroadcastReceiver {
 	            	sb.append(line + "\n");
 	            }
 	            is.close();
+	            
 	            result=sb.toString();
-	            result1=result;
+	            numofPics = Integer.parseInt(result.trim());
+	          //  result1=result;
 	            Cabron=Integer.parseInt(result.trim());
 	           /* while (result.length()!=0)
 	            {
@@ -126,7 +166,7 @@ public class DownImages extends BroadcastReceiver {
 	            	Cabron++;
 	            }*/
 	            //j=j%j.
-	            return result;
+	            return numofPics;
 	    }
 	    catch(Exception e)
 	    {
@@ -134,7 +174,7 @@ public class DownImages extends BroadcastReceiver {
 
 	            Log.e("log_tag", "Error converting result "+e.toString());
 	    }
-	     return single[j];
+	     return numofPics;
 	    //return result;
 	}   
 	private String getServerData(String returnString) 
@@ -176,7 +216,8 @@ public class DownImages extends BroadcastReceiver {
 	            result1=result;
 	            while (result.length()!=0)
 	            {
-	            	single[i] = result1.substring(0, result1.indexOf(","));
+	            	picNames.add(result1.substring(0, result1.indexOf(",")));
+	            	//single[i] = result1.substring(0, result1.indexOf(","));
 	            	result1 = result1.substring(result1.indexOf(",")+1, result1.length()); 
 	            	i++;
 	            }
@@ -188,10 +229,10 @@ public class DownImages extends BroadcastReceiver {
 	            Log.e("log_tag", "Error converting result "+e.toString());
 	    }
 	     return single[j];
-	    //return result;
+	   
 	}   
     private Drawable LoadImageFromWebOperations(String url)
-	   {
+	{
 	        try
 	        {
 	            InputStream is = (InputStream) new URL(url).getContent();
@@ -201,43 +242,46 @@ public class DownImages extends BroadcastReceiver {
 	            System.out.println("Exc="+e);
 	            return null;
 	        }
-	    }
-	
-	 private void changeURLStr(String newUrl,Context context) {
+	}
+	 private void changeURLStr(String image,Context context) {
 			//URL reviewImageURL;
-			Toast.makeText(context, "change", Toast.LENGTH_LONG).show();
+			Toast.makeText(context, "changeURLStr", Toast.LENGTH_LONG).show();
 
-			String name = reviewImageLink.substring(reviewImageLink
-					.lastIndexOf("/") + 1);
-			try {
-				reviewImageURL = new URL(newUrl);
-				if (!hasExternalStoragePublicPicture(name)) 
+			String name =url +  image;
+			try
+			{
+				Toast.makeText(context, "PicName "+ image, Toast.LENGTH_LONG).show();
+
+				reviewImageURL = new URL((name));
+					if (!hasExternalStoragePublicPicture(image)) 
 				{
+					Toast.makeText(context, "InsideIFStatement", Toast.LENGTH_LONG).show();
 					isImage = false;
 					new DownloadImageTask().execute(reviewImageURL);
 					Log.v("log_tag", "if");
 					isImage = true;
-					File sdImageMainDirectory = new File(Environment
+					/*File sdImageMainDirectory = new File(Environment
 							.getExternalStorageDirectory(), "Download");
 					sdImageMainDirectory.mkdirs();
-					File file = new File(sdImageMainDirectory, name);
+					File file = new File(sdImageMainDirectory, name);*/
 					Log.v("log_tag", "Directory created");
 				}
 
-			} catch (MalformedURLException e) {
+			} 
+			catch (MalformedURLException e) {
 				Log.v(TAG, e.toString());
 			}
 		} 
 	private class DownloadImageTask extends AsyncTask<URL, Integer, Bitmap>
 	{
+
 		// This class definition states that DownloadImageTask will take String
 		// parameters, publish Integer progress updates, and return a Bitmap
 		protected Bitmap doInBackground(URL... paths) {
 			URL url;
 			try {
 				url = paths[0];
-				HttpURLConnection connection = (HttpURLConnection) url
-						.openConnection();
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 				int length = connection.getContentLength();
 				InputStream is = (InputStream) url.getContent();
 				byte[] imageData = new byte[length];
@@ -256,8 +300,7 @@ public class DownImages extends BroadcastReceiver {
 					downloaded += read;
 					publishProgress((downloaded * 100) / length);
 				}
-				Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0,
-						length);
+				Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0,length);
 				if (bitmap != null) {
 					Log.i(TAG, "Bitmap created");
 				} else {
@@ -274,22 +317,46 @@ public class DownImages extends BroadcastReceiver {
 			}
 			return null;
 		}
-			/*protected void onPostExecute(Bitmap result)
-			{
-			String name = reviewImageLink.substring(reviewImageLink
-					.lastIndexOf("/") + 1);
-			if (result != null) {
-				hasExternalStoragePublicPicture(name);
-				saveToSDCard(result, name);
-				isImage = true;
+		
 
-			} else {
-				isImage = false;
+			@Override
+		protected void onPreExecute()
+		{
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			go=false;
+			Toast.makeText(newContext, "onPreExecute", Toast.LENGTH_LONG).show();
+		}
 
-			}
-		}*/
+
+	protected void onPostExecute(Bitmap result)
+	{
+		
+		//String name = getServerData(KEY_121);
+		String name = picNames.get(savePic);
+		savePic--;
+		Toast.makeText(newContext, "NameOfBitmap"+ result.toString(), Toast.LENGTH_LONG).show();
+
+		if(isCancelled())
+			result=null;
+		if (result != null)
+		{
+			hasExternalStoragePublicPicture(name);
+			saveToSDCard(result, name);
+			isImage = true;		
+			Toast.makeText(newContext, "if", Toast.LENGTH_LONG).show();
+		}
+		else 
+		{
+			isImage = false;
+			Toast.makeText(newContext, "else", Toast.LENGTH_LONG).show();
+		}
+			go=true;
 	}
-	/*public void saveToSDCard(Bitmap bitmap, String name) {
+	}
+	public void saveToSDCard(Bitmap bitmap, String name) {
+		Toast.makeText(newContext, "in SaveToSDCard", Toast.LENGTH_LONG).show();
+
 		boolean mExternalStorageAvailable = false;
 		boolean mExternalStorageWriteable = false;
 		String state = Environment.getExternalStorageState();
@@ -308,11 +375,14 @@ public class DownImages extends BroadcastReceiver {
 			Log.v(TAG, "Please insert a SD Card to save your Video "
 					+ mExternalStorageAvailable + mExternalStorageWriteable);
 		}
-	}*/
+	}
 
-/*	private void saveFile(Bitmap bitmap, String name) 
+	private void saveFile(Bitmap bitmap, String name) 
 	{
+
 		String filename = name;
+		Toast.makeText(newContext, "in SaveFile "+filename, Toast.LENGTH_LONG).show();
+
 		ContentValues values = new ContentValues();
 		File sdImageMainDirectory = new File(Environment
 				.getExternalStorageDirectory(), "Download");
@@ -323,13 +393,11 @@ public class DownImages extends BroadcastReceiver {
 		values.put(MediaStore.MediaColumns.DATE_ADDED, System
 				.currentTimeMillis());
 		values.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
-		Uri uri = this.getContentResolver().insert(
-				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-
-				values);
+		//Uri uri = this.getContentResolver().insert(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+		Uri uri = newContext.getContentResolver().insert(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
 		try {
-			OutputStream outStream = this.getContentResolver()
-					.openOutputStream(uri);
+		//	OutputStream outStream = this.getContentResolver().openOutputStream(uri);
+			OutputStream outStream = newContext.getContentResolver().openOutputStream(uri);
 			bitmap.compress(Bitmap.CompressFormat.PNG, 95, outStream);
 
 			outStream.flush();
@@ -339,20 +407,126 @@ public class DownImages extends BroadcastReceiver {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}*/
-	private boolean hasExternalStoragePublicPicture(String name) {
-		File sdImageMainDirectory = new File(Environment
-				.getExternalStorageDirectory(), "Download");
+	}
+	private boolean hasExternalStoragePublicPicture(String name)
+	{
+		File sdImageMainDirectory = new File(Environment.getExternalStorageDirectory(), "Download");
 		File file = new File(sdImageMainDirectory, name);
-		if (file != null) {
+		if (file != null) 
+		{
 			file.delete();
 		}
 		return file.exists();
 	}
+	
 	 public static Drawable getDrawable()
 	{
 		return drawable;
 	}
-    
-	
+//***************************************************************************
+	    /*
+	     * Cache-related fields and methods.
+	     * 
+	     * We use a hard and a soft cache. A soft reference cache is too aggressively cleared by the
+	     * Garbage Collector.
+	     */
+	    
+	    private static final int HARD_CACHE_CAPACITY = 10;
+	    private static final int DELAY_BEFORE_PURGE = 10 * 1000; // in milliseconds
+
+
+	    /**
+	     * Clears the image cache used internally to improve performance. Note that for memory
+	     * efficiency reasons, the cache will automatically be cleared after a certain inactivity delay.
+	     */
+
+	    // Hard cache, with a fixed maximum capacity and a life duration
+	    private final HashMap<String, Bitmap> sHardBitmapCache =
+	        new LinkedHashMap<String, Bitmap>(HARD_CACHE_CAPACITY / 2, 0.75f, true) {
+	        @Override
+	        protected boolean removeEldestEntry(LinkedHashMap.Entry<String, Bitmap> eldest) {
+	            if (size() > HARD_CACHE_CAPACITY) {
+	                // Entries push-out of hard reference cache are transferred to soft reference cache
+	                sSoftBitmapCache.put(eldest.getKey(), new SoftReference<Bitmap>(eldest.getValue()));
+	                return true;
+	            } else
+	                return false;
+	        }
+	    };
+
+	    // Soft cache for bitmaps kicked out of hard cache
+	    private final static ConcurrentHashMap<String, SoftReference<Bitmap>> sSoftBitmapCache =
+	        new ConcurrentHashMap<String, SoftReference<Bitmap>>(HARD_CACHE_CAPACITY / 2);
+
+	    private final Handler purgeHandler = new Handler();
+
+	    private final Runnable purger = new Runnable() {
+	        public void run() {
+	            clearCache();
+	        }
+	    };
+
+	    /**
+	     * Adds this bitmap to the cache.
+	     * @param bitmap The newly downloaded bitmap.
+	     */
+	    private void addBitmapToCache(String url, Bitmap bitmap) {
+	        if (bitmap != null) {
+	            synchronized (sHardBitmapCache) {
+	                sHardBitmapCache.put(url, bitmap);
+	            }
+	        }
+	    }
+
+	    /**
+	     * @param url The URL of the image that will be retrieved from the cache.
+	     * @return The cached bitmap or null if it was not found.
+	     */
+	    private Bitmap getBitmapFromCache(String url) {
+	        // First try the hard reference cache
+	        synchronized (sHardBitmapCache) {
+	            final Bitmap bitmap = sHardBitmapCache.get(url);
+	            if (bitmap != null) {
+	                // Bitmap found in hard cache
+	                // Move element to first position, so that it is removed last
+	                sHardBitmapCache.remove(url);
+	                sHardBitmapCache.put(url, bitmap);
+	                return bitmap;
+	            }
+	        }
+
+	        // Then try the soft reference cache
+	        SoftReference<Bitmap> bitmapReference = sSoftBitmapCache.get(url);
+	        if (bitmapReference != null) {
+	            final Bitmap bitmap = bitmapReference.get();
+	            if (bitmap != null) {
+	                // Bitmap found in soft cache
+	                return bitmap;
+	            } else {
+	                // Soft reference has been Garbage Collected
+	                sSoftBitmapCache.remove(url);
+	            }
+	        }
+
+	        return null;
+	    }
+	 
+	    /**
+	     * Clears the image cache used internally to improve performance. Note that for memory
+	     * efficiency reasons, the cache will automatically be cleared after a certain inactivity delay.
+	     */
+	    public void clearCache() {
+	        sHardBitmapCache.clear();
+	        sSoftBitmapCache.clear();
+	    }
+
+	    /**
+	     * Allow a new delay before the automatic cache clear is done.
+	     */
+	    private void resetPurgeTimer() {
+	        purgeHandler.removeCallbacks(purger);
+	        purgeHandler.postDelayed(purger, DELAY_BEFORE_PURGE);
+	    }
+
+
 }//end of class
