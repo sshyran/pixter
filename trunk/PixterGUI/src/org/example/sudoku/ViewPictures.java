446ceb7b1f78003337fcd811dc57e89a38929a3e
sanchez985556 <sanchez985556@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-04-10 19:51:49 +0000

    The GUI now shows all the pictures scrolling

--- trunk/PixterGUI/src/org/example/sudoku/ViewPictures.java
+++ trunk/PixterGUI/src/org/example/sudoku/ViewPictures.java
@@ -3,6 +3,7 @@ package org.example.sudoku;
 //import org.example.sudoku.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -13,7 +14,8 @@ public class ViewPictures extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);
+        setContentView(R.layout.viewpictures);
+        startActivity(new Intent(ViewPictures.this, image.class));
             
     }
 }
