c44baa47cf10854a7af558b9d2b8b06967791c00
sanchez985556 <sanchez985556@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-04-10 19:51:49 +0000

    The GUI now shows all the pictures scrolling

--- PixterGUI/src/org/example/sudoku/MainScreen.java
+++ PixterGUI/src/org/example/sudoku/MainScreen.java
@@ -13,6 +13,7 @@ public class MainScreen extends Activity
 {  
 Button b1;
 Button b2;
+Button b3;
 
     /** Called when the activity is first created. */
     @Override
@@ -24,17 +25,27 @@ Button b2;
         b2 = (Button) findViewById(R.id.options_button);
         b1.setOnClickListener(myhandler1);
         b2.setOnClickListener(myhandler2);
+        
+//        setContentView(R.layout.viewpictures); 
+//        b3 = (Button) findViewById(R.id.next_pic);
+//        b2.setOnClickListener(myhandler3);
+        
       }
       View.OnClickListener myhandler1 = new View.OnClickListener() {
         public void onClick(View v) {
-        	startActivity(new Intent(MainScreen.this, ViewPictures.class));
+        	startActivity(new Intent(MainScreen.this, image.class));
+ //       	startActivity(new Intent(MainScreen.this, image.class));
         }
       };
       View.OnClickListener myhandler2 = new View.OnClickListener() {
         public void onClick(View v) {
-        	startActivity(new Intent(MainScreen.this, ViewPictures.class));
-        };
-        
-    };
+        	startActivity(new Intent(MainScreen.this, Options.class));
+        }
+      };
+//      View.OnClickListener myhandler3 = new View.OnClickListener() {
+//          public void onClick(View v) {
+//          	startActivity(new Intent(MainScreen.this, image.class));
+//          }
+//        };
 
    }
