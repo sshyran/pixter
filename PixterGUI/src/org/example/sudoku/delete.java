391fac91a23f79b8a4dcd4abe93795eed99e31da
sanchez985556 <sanchez985556@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-04-17 22:02:27 +0000

    Slide Show, Delete Pictures, Download Pictures

--- PixterGUI/src/org/example/sudoku/delete.java
+++ PixterGUI/src/org/example/sudoku/delete.java
@@ -3,18 +3,19 @@ package org.example.sudoku;
 //import org.example.sudoku.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
-public class Options extends Activity 
+public class delete extends Activity 
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);
- //       startActivity(new Intent(Options.this, .class));
+        setContentView(R.layout.deletepix);
+        startActivity(new Intent(delete.this, pixDel.class));
             
     }
 }
