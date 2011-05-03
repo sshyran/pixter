17f8de721b6437e4a722d5e8e594b27fce68042c
saderfox <saderfox@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-05-03 05:55:35 +0000

    Send emails and SMS

--- AndroidEmailer/src/android/emailer/AndroidEmailer.java
+++ AndroidEmailer/src/android/emailer/AndroidEmailer.java
@@ -7,13 +7,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.RadioButton;
 
 public class AndroidEmailer extends Activity {
 	
 	private EditText mEmailTo;
     private EditText mSubject;
     private EditText mBody;
+    
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,12 @@ public class AndroidEmailer extends Activity {
         mSubject = (EditText) findViewById(R.id.emailsubject);
         mBody = (EditText) findViewById(R.id.emailtext);
         
+        rb1=(RadioButton)findViewById(R.id.radio0);
+        rb2=(RadioButton)findViewById(R.id.radio1);
+        rb3=(RadioButton)findViewById(R.id.radio2);
+        
         Button send = (Button) this.findViewById(R.id.send);
+        Button sms = (Button) this.findViewById(R.id.sms);
         
         send.setOnClickListener(new View.OnClickListener() {
 
@@ -33,6 +42,35 @@ public class AndroidEmailer extends Activity {
             	String EmailTo = mEmailTo.getText().toString();
                 String Subject = mSubject.getText().toString();
                 String Body = mBody.getText().toString();
+                try {   
+                    GMailSender sender = new GMailSender("carbertspam@gmail.com", "pixter4ever");
+                    sender.sendMail(Body,   
+                    		Subject,   
+                            "saderfox@gmail.com",   
+                            EmailTo);
+                    
+           		 
+                } catch (Exception e) {   
+                    Log.e("SendMail", e.getMessage(), e);   
+                } 
+
+            }
+        });
+        
+        sms.setOnClickListener(new View.OnClickListener() {
+        	
+        	
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+            	String EmailTo = mEmailTo.getText().toString();
+                String Subject = mSubject.getText().toString();
+                String Body = mBody.getText().toString();
+                if(rb1.isChecked() == true)
+                	EmailTo = EmailTo + "@txt.att.net";
+                if(rb2.isChecked() == true)
+                	EmailTo = EmailTo + "@messaging.sprintpcs.com";
+                if(rb3.isChecked() == true)
+                	EmailTo = EmailTo + "@vtext.com";
                 try {   
                     GMailSender sender = new GMailSender("carbertspam@gmail.com", "pixter4ever");
                     sender.sendMail(Body,   
