d9990497b67cd898aad671ea2094c092040a1c17
sanchez985556 <sanchez985556@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-05-15 23:46:15 +0000

    better options menu with some newer buttons on manageimages and downloadimages Background was changed, probably not a great one.

--- trunk/PixterGUI/src/org/example/sudoku/transitionSpeed.java
+++ trunk/PixterGUI/src/org/example/sudoku/transitionSpeed.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
   
 public class transitionSpeed extends Activity {  
 	 	private RadioButton rb0;
@@ -17,7 +18,14 @@ public class transitionSpeed extends Activity {
 	public static long interval_speed = 2000;
 	public static long slide;
 	public static boolean fade;
-	@Override  
+	
+    public static String transition;
+    public static String animation;
+	@Override 
+	public void onBackPressed()
+    { 
+    	startActivity(new Intent(transitionSpeed.this, MainScreen.class));
+    }
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.transitionspeed);
@@ -32,19 +40,28 @@ public class transitionSpeed extends Activity {
         Save.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
         		startActivity(new Intent(transitionSpeed.this, MainScreen.class));
+        		 
                 // TODO Auto-generated method stub
             	if(rb0.isChecked() == true)
             		interval_speed = 3000;
+            		transition = "Last Interval speed : 3 Sec";
                 if(rb1.isChecked() == true)
                 	interval_speed = 4000;
+                	transition = "Last Interval speed : 4 Sec";
                 if(rb2.isChecked() == true)
                 	interval_speed = 5000;
+                	transition= "Last Interval speed : 5 Sec";
                 if(rb3.isChecked() == true)
                 	slide = 1000;
+                	animation ="Last Animation : Slide in/out";
                 if(rb4.isChecked() == true)
                 	slide = 2000;
+                	animation ="Last Animation : Fade in/out";
+                	
         	}
         	});
 	    }
+	
+	
 	}
 
