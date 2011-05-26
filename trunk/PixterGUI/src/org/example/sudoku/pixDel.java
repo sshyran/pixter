032a5bb151f870580cde542c203be2e2c4dbab1f
sanchez985556 <sanchez985556@90c0385d-1514-19d8-ad91-3150c3d9de13>
2011-05-26 17:28:09 +0000

    all the buttons work now

--- trunk/PixterGUI/src/org/example/sudoku/pixDel.java
+++ trunk/PixterGUI/src/org/example/sudoku/pixDel.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -37,28 +36,10 @@ public class pixDel extends Activity implements OnClickListener {
         Button btn = (Button) findViewById(R.id.Button01);
         registerForContextMenu(btn);
         btn.setOnClickListener(DeleteMenu);
-        
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setMessage("No Images in Media Storage. Do you wish to download Images?")
-    	       .setCancelable(false)
-    	       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-    	           public void onClick(DialogInterface dialog, int id) {
-    	        	   if (login.user != null)
-    	        		   startActivity(new Intent(pixDel.this, image.class));
-    	        	   else
-    	        		   startActivity(new Intent(pixDel.this, login.class));
-    	           }
-    	       })
-    	       .setNegativeButton("No", new DialogInterface.OnClickListener() {
-    	           public void onClick(DialogInterface dialog, int id) {
-    	        	   startActivity(new Intent(pixDel.this, MainScreen.class));
-    	           }
-    	       });
-    	AlertDialog alert = builder.create();
 
         //View v = (View)findViewById(R.id.view); 
         //v.setOnClickListener(this);
-        try{
+
 	    imageNames = ReadSDCard();										//Read Images from SD card and store in List
 	    imgView =(ImageView)findViewById(R.id.ImageView01);				// Instantiate Image view
 	    imgView.setImageDrawable(Drawable.createFromPath(imageNames     // View images from List
@@ -74,10 +55,7 @@ public class pixDel extends Activity implements OnClickListener {
 	    flip.setOutAnimation(this, android.R.anim.fade_out);
 	    
 	  //  refreshUI();	//Handler to refresh Imageview every 3 seconds
-	    }catch(Exception e){
-	    	builder.show();
-	    	
-	    }
+	    
 	   
 	}  
 	
